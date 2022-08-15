11f4209c20b7b08afa1e0a54ccf8150f3275cd79
JD-ES <63867919+JD-ES@users.noreply.github.com>
2022-08-15 12:22:04 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. realizado por Juan Deyvis. 
+  Welcome to ${prefix}'s app. realizado por Juan Deyvis. 
   <!-- END -->
   
   </div>
