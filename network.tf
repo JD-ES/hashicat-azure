9da1204c9852e9a18866ff63bbc9d8281b1bdb0c
JD-ES <63867919+JD-ES@users.noreply.github.com>
2022-08-15 14:01:06 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -5,10 +5,10 @@ resource "azurerm_resource_group" "example" {
 module "network-1" {
   source  = "app.terraform.io/juan_org/network-1/azurerm"
   version = "3.5.0"
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
   
-    depends_on = [azurerm_resource_group.example]
+    depends_on = [azurerm_resource_group.myresourcegroup]
 }
