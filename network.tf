88110561dfb027c640113b88467772763c34b452
JD-ES <63867919+JD-ES@users.noreply.github.com>
2022-08-15 13:53:48 -0500

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "example" {
   name     = "my-resources"
-  location = "East US"
+  location = var.location
 }
 module "network-1" {
   source  = "app.terraform.io/juan_org/network-1/azurerm"
@@ -9,4 +9,6 @@ module "network-1" {
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
+  
+    depends_on = [azurerm_resource_group.example]
 }
